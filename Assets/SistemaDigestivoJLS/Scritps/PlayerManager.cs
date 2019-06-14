6d52aa8783ed66e294c9594257c5a0701a4d379c
TecnoParqueLuis <jlsuarique@gmail.com>
2019-06-14 17:01:31 -0500

    P6JLS

---
+++ Assets/SistemaDigestivoJLS/Scritps/PlayerManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public GameObject PrefabText;
+    public bool Texto = false;
+
+    private void Start()
+    {
+        TextLoand();
+
+    }
+
+    public void TextLoand()
+    {
+
+        
+        GameObject texto = Instantiate(PrefabText);
+        texto.transform.position = new Vector3(this.gameObject.transform.position.x + 5,
+        this.gameObject.transform.position.y + 2, this.gameObject.transform.position.z);
+        texto.transform.SetParent(this.transform);
+        Debug.Log("Apesco en la portada Auuuuu");
+    }
+
+    IEnumerator TextoAparece(float time)
+    {
+
+
+        yield return new WaitForSeconds(time);
+
+    }
+
+    
+}
