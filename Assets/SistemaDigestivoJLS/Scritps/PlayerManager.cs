62444c969dd532aa423050e8495727be45db2eb5
TecnoParqueLuis <jlsuarique@gmail.com>
2019-06-21 16:55:33 -0500

    P7JLS, realice una animacion del para texto del personaje al incio del juego

--- Assets/SistemaDigestivoJLS/Scritps/PlayerManager.cs
+++ Assets/SistemaDigestivoJLS/Scritps/PlayerManager.cs
@@ -9,28 +9,6 @@ public class PlayerManager : MonoBehaviour
 
     private void Start()
     {
-        TextLoand();
 
     }
-
-    public void TextLoand()
-    {
-
-        
-        GameObject texto = Instantiate(PrefabText);
-        texto.transform.position = new Vector3(this.gameObject.transform.position.x + 5,
-        this.gameObject.transform.position.y + 2, this.gameObject.transform.position.z);
-        texto.transform.SetParent(this.transform);
-        Debug.Log("Apesco en la portada Auuuuu");
-    }
-
-    IEnumerator TextoAparece(float time)
-    {
-
-
-        yield return new WaitForSeconds(time);
-
-    }
-
-    
 }
