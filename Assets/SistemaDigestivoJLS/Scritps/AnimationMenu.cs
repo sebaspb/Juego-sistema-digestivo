a578feff0613afc78d19eed404f426f81826d4f1
TecnoParqueLuis <jlsuarique@gmail.com>
2019-07-26 16:33:14 -0500

    P9.6JLS: se implemento el video del menu

---
+++ Assets/SistemaDigestivoJLS/Scritps/AnimationMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class AnimationMenu : MonoBehaviour
+{
+    public RawImage image;
+    public VideoPlayer video; 
+
+    void Start()
+    {
+        StartCoroutine(playVideo());
+    }
+
+    IEnumerator playVideo()
+    {
+        video.Prepare();
+        WaitForSeconds WFS = new WaitForSeconds(1);
+        while (!video.isPrepared)
+        {
+            yield return WFS;
+            break;
+        }
+        image.texture = video.texture;
+        video.Play();
+    }
+
+}
