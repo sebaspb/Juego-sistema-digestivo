da64a742d27ec17db3cec5075f190374f16793dd
TecnoParqueLuis <jlsuarique@gmail.com>
2019-07-29 15:40:18 -0500

    P9.7JLS: se arreglo el problema del script SceneManager y se le agrego una nuve de texto a la escene del minijuego de la boca.

--- Assets/SistemaDigestivoJLS/Scritps/SceneGameManager.cs
+++ Assets/SistemaDigestivoJLS/Scritps/SceneGameManager.cs
@@ -6,63 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneGameManager : MonoBehaviour
 {
-    public GameObject PanelSettings;
-    public GameObject PanelOption;
-
-    public GameObject Open;
-    public GameObject Close;
-    public GameObject OpOpen;
-    public GameObject OpClos;
-
-    private Animator menuAnim;
-    private Animator OptionAnim;
-
-    void Awake()
-    {
-        menuAnim = PanelSettings.GetComponent<Animator>();
-        OptionAnim = PanelOption.GetComponent<Animator>();
-    }
-
-    public void SettingsOne()
-    {
-        Open.SetActive(true);
-        Close.SetActive(false);
-        Open.SetActive(false);
-        Close.SetActive(true);
-        menuAnim.SetBool("Open",true);
-        menuAnim.SetBool("Close",false);    
-    }
-
-    public void SettingsTwo()
-    {
-        Open.SetActive(false);
-        Close.SetActive(true);
-        Open.SetActive(true);
-        Close.SetActive(false);
-        menuAnim.SetBool("Close", true);
-        menuAnim.SetBool("Open",false);   
-    }
-
-    public void OptionOP()
-    {
-        OpOpen.SetActive(true);
-        OpClos.SetActive(false);
-        OpOpen.SetActive(false);
-        OpClos.SetActive(true);
-        OptionAnim.SetBool("Open",true);
-        OptionAnim.SetBool("Close",false);
-    }
-
-    public void OptionClo()
-    {
-        OpOpen.SetActive(false);
-        OpClos.SetActive(true);
-        OpOpen.SetActive(true);
-        OpClos.SetActive(false);
-        OptionAnim.SetBool("Close", true);
-        OptionAnim.SetBool("Open", false);
-    }
-
     public void CargarScena(string nombreScene)
     {
         SceneManager.LoadScene(nombreScene);
