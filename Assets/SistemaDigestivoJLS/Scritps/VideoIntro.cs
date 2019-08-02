a1a9465d00882824e2bc7b7200409456355b11df
SistemaDigestivo <jlsuarique@gmail.com>
2019-08-02 13:12:22 -0500

    P10.1JLS: se creo el intro del juego.

---
+++ Assets/SistemaDigestivoJLS/Scritps/VideoIntro.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+
+public class VideoIntro : MonoBehaviour
+{
+
+    public RawImage image;
+    public VideoPlayer video;
+
+    void Start()
+    {
+        StartCoroutine(TimeScene());
+    }
+
+    IEnumerator playVideo()
+    {
+        video.Prepare();
+        WaitForSeconds WFS = new WaitForSeconds(0.15f);
+        while (!video.isPrepared)
+        {
+            yield return WFS;
+            break;
+        }
+        image.texture = video.texture;
+        video.Play();
+    }
+    IEnumerator TimeScene()
+    {
+        yield return new WaitForSeconds(6);
+        SceneManager.LoadScene(1);
+        StartCoroutine(playVideo());
+    }
+}
